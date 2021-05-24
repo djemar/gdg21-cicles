80e0a22bfeffb5e0d4550a51ee5c1766879426a9
Enrico D'Oro <er.enricodoro@gmail.com>
2021-05-24 11:54:51 +0200

    add instance for not reloading theme

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -5,10 +5,19 @@ using System;
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
+    public static AudioManager instance;
 
     // Use this for initialization
     void Awake()
     {
+        if(instance == null)
+            instance = this;
+        else{
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
