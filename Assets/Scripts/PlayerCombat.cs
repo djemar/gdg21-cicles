1dcd1f33c33721533cd2f4c355ae8fa7a0e212a0
Enrico D'Oro <er.enricodoro@gmail.com>
2021-05-28 02:15:40 +0200

    add melee attack with hit box and animation

---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+    private bool isAttacking = false;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isAttacking)
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+        //animator.SetLayerWeight(animator.GetLayerIndex("Attack Layer"), 1);
+        animator.SetTrigger("Attack");
+        isAttacking = false;
+
+        yield return new WaitForSeconds(0.5f);
+        Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider enemy in hitEnemies)
+        {
+            Destroy(enemy.gameObject);
+        }
+        //animator.SetLayerWeight(animator.GetLayerIndex("Attack Layer"), 0);
+    }
+
+    public void OnAttack(InputAction.CallbackContext value)
+    {
+        if (value.started && !isAttacking)
+        {
+            isAttacking = true;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
