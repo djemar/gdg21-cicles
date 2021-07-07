a745391f5c9d53d06c1a47d89b8915ce3b81b446
Alessandro Salvatore <s275651@studenti.polito.it>
2021-07-07 17:17:19 +0200

    fix: aggiustate le icone nell'hud

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -11,46 +11,92 @@ public class PlatformMovement : MonoBehaviour
     private float endPosition;
     private bool rising;
     private bool descending;
+    private bool left;
+    private bool right;
+    public bool ascending;
+    public bool horizontal;
 
     void Start()
     {
-        startPosition = transform.position.y;
-        endPosition = startPosition + 20;
+        if (ascending)
+        {
+            startPosition = transform.position.y;
+            endPosition = startPosition + 20;
+        }
+        else
+        {
+            endPosition = transform.position.y;
+            startPosition = endPosition - 20;
+        }
+        if (horizontal)
+        {
+            startPosition = transform.position.z;
+            endPosition = startPosition + 20;
+        }
         rising = false;
         descending = false;
+        left = false;
+        right = false;
     }
 
     void FixedUpdate()
     {
-        if (!rising && !descending) rising = true;
-        if (rising)
+        if (!horizontal)
         {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, endPosition, transform.position.z), 3 * Time.deltaTime);
-            if(transform.position.y >= endPosition) { 
-                rising = false;
-                descending = true;
+            if (!rising && !descending) if (ascending) rising = true; else descending = true;
+            if (rising)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, endPosition, transform.position.z), 3 * Time.deltaTime);
+                if (transform.position.y >= endPosition)
+                {
+                    rising = false;
+                    descending = true;
+                }
+            }
+            if (descending)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, startPosition, transform.position.z), 3 * Time.deltaTime);
+                if (transform.position.y <= startPosition)
+                {
+                    rising = true;
+                    descending = false;
+                }
             }
         }
-        if(descending)
+        else
         {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, startPosition, transform.position.z), 3 * Time.deltaTime);
-            if (transform.position.y <= startPosition)
+            if (!left && !right) right = true;
+            if(right)
             {
-                rising = true;
-                descending = false;
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, endPosition), 3 * Time.deltaTime);
+                if (transform.position.z >= endPosition)
+                {
+                    right = false;
+                    left = true;
+                }
+            }
+            if(left)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, startPosition), 3 * Time.deltaTime);
+                if (transform.position.z <= startPosition)
+                {
+                    right = true;
+                    left = false;
+                }
             }
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
+        Debug.Log("Pisciati addosso");
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.transform.parent = transform;
         }
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider collision)
     {
         if (collision.gameObject.CompareTag("Player"))
             collision.transform.parent = null;
